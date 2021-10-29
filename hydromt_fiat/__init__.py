8347bef82841f43ac32fb1e57cb9b4b95329848a
LaurensLeunge <67266307+LaurensLeunge@users.noreply.github.com>
2021-10-29 15:09:42 +0200

    Update __init__.py

--- hydromt_fiat/__init__.py
+++ hydromt_fiat/__init__.py
@@ -3,7 +3,7 @@
 from os.path import dirname, join, abspath
 
 # NOTE version number without "v"
-__version__ = "0.2.0.dev"
+__version__ = "0.2.0"
 
 DATADIR = join(dirname(abspath(__file__)), "data")
 
