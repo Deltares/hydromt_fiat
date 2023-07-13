dcfd4a96c98508adc32abd393bd6c5da420a2e16
Mares2022 <mario.fuentesmonjaraz@deltares.nl>
2023-07-13 13:23:06 +0200

    Updating test_integration_hazard.py with multiple inputs (geotif, sfincs output)
    Updating test_integration_hazard.py with multiple inputs (geotif, sfincs output)

--- tests/test_integrations_hazard.py
+++ tests/test_integrations_hazard.py
@@ -25,84 +25,72 @@ def test_hazard(case):
     if root.exists:
         shutil.rmtree(root)
 
-    # configuration = {
-    #     "setup_hazard": {
-    #         "map_fn":   ["C:/Users/fuentesm/CISNE/Deltares/Delft-FIAT/test/tmp/overland/sfincs_map.nc"], #, "C:/Users/fuentesm/CISNE/Deltares/Delft-FIAT/test/tmp/overland/sfincs_map.nc"],       # ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth","Current_prob_event_set_combined_doNothing_withSeaWall_RP=50_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=100_max_flood_depth"]                         #map_fn   = ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=5_max_flood_depth"]					    		# absolute or relative (with respect to the configuration.ini) path to the hazard file
-    #         "map_type": "water_depth",                                                             # description of the hazard file type
-    #         "rp":       None,            				    	                              # hazard return period in years, required for a risk calculation (optional)
-    #         "crs":      None,  						                              # coordinate reference system of the hazard file (optional)
-    #         "nodata":   -99999,             				                                      # value that is assigned as nodata (optional)
-    #         "var":      "zsmax",							                              # hazard variable name in NetCDF input files (optional)
-    #         "chunks":   "auto",  						                              # chunk sizes along each dimension used to load the hazard file into a dask array (default is 'auto') (optional)
-    #         "name_catalog": None,
-    #         "risk_output": False,	
-    #     }
-    # }
-
-    # configuration = {
-    #     "setup_hazard": {   
-    #         "map_fn":   ["P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth.tif", "P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth.tif"],       # ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth","Current_prob_event_set_combined_doNothing_withSeaWall_RP=50_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=100_max_flood_depth"]                         #map_fn   = ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=5_max_flood_depth"]					    		# absolute or relative (with respect to the configuration.ini) path to the hazard file
-    #         "map_type": "water_depth",                                                             # description of the hazard file type
-    #         "rp":       None,            				    	                              # hazard return period in years, required for a risk calculation (optional)
-    #         "crs":      None,  						                              # coordinate reference system of the hazard file (optional)
-    #         "nodata":   -99999,             				                                      # value that is assigned as nodata (optional)
-    #         "var":      None,							                              # hazard variable name in NetCDF input files (optional)
-    #         "chunks":   "auto",  						                              # chunk sizes along each dimension used to load the hazard file into a dask array (default is 'auto') (optional)
-    #         "name_catalog": "flood_maps",
-    #         "risk_output": True,	
-    #     }
-    # }
-
-
-    mode = "risk"
-    map_path = Path("C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/")
-    # mode = "single"
-    # map_path = Path("C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_extreme12ft_no_measures/overland")
-
-    map_fn = []
-
-    if mode == "risk":
-        # check for netcdf
-        for file in os.listdir(str(map_path)):
-            if file.endswith("_maps.nc"):
-                map_fn.append(map_path.joinpath(file))
-        risk_output = True
-        var = "risk_map"
-
-    elif mode == "single":
-        map_fn.append(map_path.joinpath("sfincs_map.nc"))
-        risk_output = False
-        var = "zsmax"
-
-
+    # uncomment to test event analysis from geotiff file
     configuration = {
         "setup_hazard": {   
-            "map_fn":   map_fn, #["C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=1_maps.nc", "C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=2_maps.nc", "C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=5_maps.nc"],       # ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth","Current_prob_event_set_combined_doNothing_withSeaWall_RP=50_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=100_max_flood_depth"]                         #map_fn   = ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=5_max_flood_depth"]					    		# absolute or relative (with respect to the configuration.ini) path to the hazard file
-            "map_type": "water_depth",                                                             # description of the hazard file type
-            "rp":       None,            				    	                              # hazard return period in years, required for a risk calculation (optional)
-            "crs":      None,  						                              # coordinate reference system of the hazard file (optional)
-            "nodata":   -999,             				                                      # value that is assigned as nodata (optional)
-            "var":      var,							                              # hazard variable name in NetCDF input files (optional)
-            "chunks":   "auto",  						                              # chunk sizes along each dimension used to load the hazard file into a dask array (default is 'auto') (optional)
+            "map_fn":   ["P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth.tif"],       
+            "map_type": "water_depth",                                                            
+            "rp":       None,            				    	                             
+            "crs":      None,  						                              
+            "nodata":   -99999,             				                                 
+            "var":      None,							                              
+            "chunks":   "auto",  						                             
             "name_catalog": None,
-            "risk_output":  risk_output,	
+            "risk_output": False,	
         }
     }
 
+    # uncomment to test risk analysis from geotiff file
     # configuration = {
     #     "setup_hazard": {   
-    #         "map_fn":   ["C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=1_maps.nc", "C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=2_maps.nc", "C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/RP=5_maps.nc"],       # ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth","Current_prob_event_set_combined_doNothing_withSeaWall_RP=50_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=100_max_flood_depth"]                         #map_fn   = ["Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth", "Current_prob_event_set_combined_doNothing_withSeaWall_RP=5_max_flood_depth"]					    		# absolute or relative (with respect to the configuration.ini) path to the hazard file
-    #         "map_type": "water_depth",                                                             # description of the hazard file type
-    #         "rp":       None,            				    	                              # hazard return period in years, required for a risk calculation (optional)
-    #         "crs":      None,  						                              # coordinate reference system of the hazard file (optional)
-    #         "nodata":   -99999,             				                                      # value that is assigned as nodata (optional)
-    #         "var":      "risk_maps",							                              # hazard variable name in NetCDF input files (optional)
-    #         "chunks":   "auto",  						                              # chunk sizes along each dimension used to load the hazard file into a dask array (default is 'auto') (optional)
+    #         "map_fn":   ["P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=1_max_flood_depth.tif", "P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=2_max_flood_depth.tif", "P:/11207949-dhs-phaseii-floodadapt/Model-builder/Delft-FIAT/local_test_database/data/Hazard/Current_prob_event_set_combined_doNothing_withSeaWall_RP=5_max_flood_depth.tif"],       
+    #         "map_type": "water_depth",                                                            
+    #         "rp":       None,            				    	                             
+    #         "crs":      None,  						                              
+    #         "nodata":   -99999,             				                                 
+    #         "var":      None,							                              
+    #         "chunks":   "auto",  						                             
     #         "name_catalog": None,
     #         "risk_output": True,	
     #     }
     # }
 
+    # uncomment to test event analysis from sfincs output
+    # mode = "single"
+    # map_path = Path("C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_extreme12ft_no_measures/overland")
+
+    # uncomment to test risk analysis from sfincs outputs
+    # mode = "risk"
+    # map_path = Path("C:/Users/fuentesm/CISNE/Deltares/FloodAdapt/tests/test_database/charleston/output/simulations/current_test_set_no_measures/")
+
+    # map_fn = []
+
+    # if mode == "risk":
+    #     # check for netcdf
+    #     for file in os.listdir(str(map_path)):
+    #         if file.endswith("_maps.nc"):
+    #             map_fn.append(map_path.joinpath(file))
+    #     risk_output = True
+    #     var = "risk_map"
+
+    # elif mode == "single":
+    #     map_fn.append(map_path.joinpath("sfincs_map.nc"))
+    #     risk_output = False
+    #     var = "zsmax"
+
+    # configuration = {
+    #     "setup_hazard": {   
+    #         "map_fn":   map_fn,                                                     # absolute or relative (with respect to the configuration.ini) path to the hazard file
+    #         "map_type": "water_depth",                                              # description of the hazard file type
+    #         "rp":       None,            				                              # hazard return period in years, required for a risk calculation (optional)
+    #         "crs":      None,  						                              # coordinate reference system of the hazard file (optional)
+    #         "nodata":   -999,             	                                      # value that is assigned as nodata (optional)
+    #         "var":      var,							                              # hazard variable name in NetCDF input files (optional)
+    #         "chunks":   "auto",  						                              # chunk sizes along each dimension used to load the hazard file into a dask array (default is 'auto') (optional)
+    #         "name_catalog": None,
+    #         "risk_output":  risk_output,	
+    #     }
+    # }
 
 
     logger = setuplog("hydromt_fiat", log_level=10)
@@ -110,7 +98,7 @@ def test_hazard(case):
 
     fm = FiatModel(root=root, mode="w", data_libs=[data_catalog_yml], logger=logger)
     region = fm.data_catalog.get_geodataframe("region", variables=None)
-    opt = configread(_cases[case]["ini"])
+    # opt = configread(_cases[case]["ini"])
     # fm.build(region={"geom": region}, opt=opt)
     fm.build(region={"geom": region}, opt=configuration)
     fm.write()
