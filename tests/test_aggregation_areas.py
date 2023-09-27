1adc043bf8b811435496fc2bb9b5b883466e3b0b
sarah.rautenbach <sarah.rautenbach@deltares.nl>
2023-09-27 12:45:39 +0200

    updated test for single&multipe aggregation areas

--- tests/test_aggregation_areas.py
+++ tests/test_aggregation_areas.py
@@ -11,38 +11,32 @@ from hydromt_fiat.workflows.aggregation_areas import join_exposure_aggregation_a
 from hydromt_fiat.workflows.exposure_vector import ExposureVector
 from hydromt_fiat.workflows.vulnerability import Vulnerability
 
+import shutil
+
 # set pyogrio as default engine
 gpd.options.io_engine = "pyogrio"
 
 # Load Data
-EXAMPLEDIR = Path(r"n:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model")
-# Remove after
-my_dic = {
-    "aggregation_area_fn": [
-        r"N:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model\zones.gpkg",
-        r"N:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model\zones2.gpkg",
-    ]
-}
-
+EXAMPLEDIR = Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\FIAT_model")
 
 _cases = {
     "aggregation_test_1": {
-        "new_root": r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\modelbuilder_sprint",
+        "new_root": Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\modelbuilder_sprint"),
         "configuration": {
             "setup_aggregation_areas": {
-                "aggregation_area_fn": r"N:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model\zones.gpkg",
+                "aggregation_area_fn": r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\FIAT_model\zones.gpkg",
                 "attribute_names": "ZONE_BASE",
                 "label_names": "Zoning_map",
             }
         },
     },
     "aggregation_test_2": {
-        "new_root": r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\modelbuilder_sprint2",
+        "new_root": Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\modelbuilder_sprint2"),
         "configuration": {
             "setup_aggregation_areas_multiple": {
                 "aggregation_area_fn": [
-                    r"N:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model\zones.gpkg",
-                    r"N:\Deltabox\Postbox\Rautenbach, Sarah\FIAT_model\zones2.gpkg",
+                    r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\FIAT_model\zones.gpkg",
+                    r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\FIAT\20230927_Hydromt_Fiat_Sprint\FIAT_model\zones2.gpkg",
                 ],
                 "attribute_names": ["ZONE_BASE", "LAND_USE"],
                 "label_names": ["Zoning_map", "Land_use_map"],
@@ -52,13 +46,14 @@ _cases = {
 }
 
 
+
 # Set up Fiat Model
-
-
 @pytest.mark.parametrize("case", list(_cases.keys()))
 def test_aggregation_areas(case: ParameterSet | Sequence[object] | object):
     # Read model in examples folder.
     root = EXAMPLEDIR
+    if _cases[case]["new_root"].exists():
+        shutil.rmtree(_cases[case]["new_root"])
     logger = setuplog("hydromt_fiat", log_level=10)
 
     fm = FiatModel(root=root, mode="r", logger=logger)
