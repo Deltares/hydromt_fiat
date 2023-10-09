c6b50269c1ce982e6836fd31807196801b48c73e
sarah.rautenbach <sarah.rautenbach@deltares.nl>
2023-10-09 15:19:02 +0200

    cleaning building footprint code

--- hydromt_fiat/workflows/building_footprints.py
+++ hydromt_fiat/workflows/building_footprints.py
@@ -43,7 +43,10 @@ def join_exposure_building_footprint(
     
     bf_gdf = gpd.read_file(building_footprint_fn)
 
-    ## check the projection of both gdf and if not match, reproject
+    #Add index column with default name
+    bf_gdf['B_footprint'] = range(1,len(bf_gdf)+1,1)
+
+    # check the projection of both gdf and if not match, reproject
     if exposure_gdf.crs != bf_gdf.crs:
         bf_gdf = bf_gdf.to_crs(exposure_gdf.crs)
 
@@ -52,9 +55,6 @@ def join_exposure_building_footprint(
     
     #Check for unique BF-FID
     assert bf_gdf[attribute_name].is_unique, f"Building footprint ID returns duplicates. Building footprint ID should be unique."
-
-    #Create fully merged gdf
-    #merged_gdf = gis_utils.nearest_merge(bf_gdf,exposure_gdf)
 
     # If you overwrite the exposure_gdf with the joined data, you can append all 
     # aggregation areas to the same exposure_gdf
@@ -86,21 +86,15 @@ def join_exposure_building_footprint(
     # column
     exposure_gdf[attribute_name] = exposure_gdf[attribute_name].apply(process_value)
         
-    # Rename the 'aggregation_attribute' column to 'new_column_name'. Put in 
-    # Documentation that the order the user put the label name must be the order of the gdf
+    # Rename the 'aggregation_attribute' column to 'new_column_name'
     exposure_gdf.rename(columns={attribute_name: "BF_FID"}, inplace=True)
 
-    ##remove the index_right column
-    if "index_right" in exposure_gdf.columns:
-        del exposure_gdf["index_right"]
-
     return exposure_gdf
-
 
 def join_exposure_bf(
     exposure_gdf: gpd.GeoDataFrame,
     building_footprint_fn: Union[str, Path],
-    attribute_names: str,
+    attribute_name: str,
     column_name: str = "BF_FID",
 ) -> gpd.GeoDataFrame:
     """Join building footprints to the exposure data.
@@ -111,15 +105,12 @@ def join_exposure_bf(
         Exposure data to join the building footprints to as "BF_FID".
     building_footprint_fn : Union[List[str], List[Path], str, Path]
         Path(s) to the building footprint.
-    attribute_names : Union[List[str], str]
+    attribute_name : Union[List[str], str]
         Name of the building footprint ID to join.
     column_name: str = "BF_FID"
         Name of building footprint in new exposure output
     """
-    exposure_gdf = join_exposure_building_footprint(exposure_gdf, building_footprint_fn, attribute_names)
-    
-    # Remove the geometry column from the exposure_gdf to return a dataframe
-    del exposure_gdf["geometry"]
+    exposure_gdf = join_exposure_building_footprint(exposure_gdf, building_footprint_fn, attribute_name)
     
     return exposure_gdf
 
@@ -127,44 +118,37 @@ def join_exposure_bf(
 def nearest_neighbor_bf(
     exposure_gdf: gpd.GeoDataFrame,
     building_footprint_fn: Union[str, Path],
-    attribute_names: str,
+    attribute_name: str,
     column_name: str = "BF_FID",
 ) -> gpd.GeoDataFrame:
     
-    #this should be a dataframe
-    exposure_df = join_exposure_bf(exposure_gdf, building_footprint_fn, attribute_names)
+    #Load new exposure dataframe
+    exposure_gdf = join_exposure_bf(exposure_gdf, building_footprint_fn, attribute_name)
    
-    #Load buildings.gpkg
-    exposure_gdf= gpd.read_file(Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\HydroMT\Issue_Solving\#133\testcase_data\exposure\buildings.gpkg"))
-
-    
-    #Mhere all good
-    #exposure_df= pd.read_csv(Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\HydroMT\Issue_Solving\#133\output\exposure\exposure.csv"))
-    #exposure_gdf= gpd.read_file(Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\HydroMT\Issue_Solving\#133\output\exposure\buildings.gpkg"))
-
-    
-    merged_gdf_multiple = exposure_gdf.merge(exposure_df, left_on='Object ID', right_on='Object ID', how='inner')
+    #Load building footprint
     bf_gdf = gpd.read_file(building_footprint_fn)
-    #same as above only absolute path : bf_gdf = gpd.read_file(Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\HydroMT\Issue_Solving\#133\testcase_data\building_footprints\building_footprint.gpkg"))
+   
+    #merge gdf and df > is this neccessary
+    merged_gdf = gis_utils.nearest_merge(exposure_gdf, bf_gdf, column_name)
     
-    merged_gdf = gis_utils.nearest_merge(merged_gdf_multiple, bf_gdf, "BF_FID")
-    merged_gdf.to_file(Path(r"C:\Users\rautenba\OneDrive - Stichting Deltares\Documents\Projects\HydroMT\Issue_Solving\#133\testcase_data\building_footprints\test.gpkg"))
-    value =1
-    
-    merged_gdf["index_right"] += value
+    #Correct index
+    merged_gdf["index_right"] += 1
 
     # Specify the columns with NaN values and the column to use for replacement
-    column_with_nan = "BF_FID"  # Replace with the name of the column containing NaN values
+    column_with_nan = column_name  # Replace with the name of the column containing NaN values
     replacement_column = "index_right"  # Replace with the name of the column to use for replacement
 
-    # Use the fillna method to replace NaN values in column1 with values from column2
+    # Replace NaN values in BF_FID with values from nearest neighbour
     merged_gdf[column_with_nan].fillna(merged_gdf[replacement_column], inplace=True)
 
-     ##remove the index_right column
+    #remove the index_right and distance_right column
     if "index_right" in merged_gdf.columns:
         del merged_gdf["index_right"]
+        del merged_gdf["distance_right"]
     
+    #remove geoms column to return to datafrane
     del merged_gdf["geometry"]
+    
 
     return merged_gdf
 
