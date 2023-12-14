460051ec71f7fcef3d4a3a60bb83e4c3d9464fc9
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2023-12-14 11:41:01 +0100

    allow to directly provide a DataArray for the hazard raster

--- hydromt_fiat/fiat.py
+++ hydromt_fiat/fiat.py
@@ -2,9 +2,10 @@
 
 import csv
 import logging
+import os
 from pathlib import Path
 from typing import List, Optional, Union
-
+import xarray as xr
 import geopandas as gpd
 import hydromt
 import pandas as pd
@@ -453,10 +454,17 @@ class FiatModel(GridModel):
 
         for idx, da_map_fn in enumerate(params["map_fn_lst"]):
             # read maps and retrieve their attributes
-            da_map_fn, da_name, da_type = read_maps(params, da_map_fn, idx)
-
-            da = self.data_catalog.get_rasterdataset(da_map_fn)  # removed geom=self.region because it is not always there
-
+            if isinstance(da_map_fn, os.PathLike):
+                # if path is provided read and load it as xarray
+                da_map_fn, da_name, da_type = read_maps(params, da_map_fn, idx)
+                da = self.data_catalog.get_rasterdataset(da_map_fn)  # removed geom=self.region because it is not always there
+            elif isinstance(da_map_fn, xr.DataArray):
+                # if xarray is provided directly assign that
+                da = da_map_fn
+                da_name = 'hazard_map'
+                da_type = map_type
+            else:
+                raise ValueError("The hazard map provided should be a path like object or an DataArray")
             # Convert to units of the exposure data if required
             if (
                 self.exposure in locals() or self.exposure in globals()
