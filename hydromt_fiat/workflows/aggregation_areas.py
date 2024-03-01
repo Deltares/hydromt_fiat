e172b3b0e591892d186fb20e88cc88d2a6282684
sarah.rautenbach <sarah.rautenbach@deltares.nl>
2024-03-01 14:30:25 +0100

    quickcommit

--- hydromt_fiat/workflows/aggregation_areas.py
+++ hydromt_fiat/workflows/aggregation_areas.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 from typing import List, Union
 from pathlib import Path
-
+import pandas as pd
 import numpy as np
 
 def process_value(value):
@@ -73,6 +73,7 @@ def spatial_joins(
         
         # If you overwrite the exposure_gdf with the joined data, you can append all 
         # aggregation areas to the same exposure_gdf
+       
         exposure_gdf = gpd.sjoin(
             exposure_gdf,
             area_gdf,
@@ -91,20 +92,52 @@ def spatial_joins(
             exposure_gdf.groupby("Object ID")[attribute_name].agg(list).reset_index()
         )
         exposure_gdf.drop_duplicates(subset="Object ID", keep="first", inplace=True)
-        exposure_gdf.drop(columns=attribute_name, inplace=True)
-        exposure_gdf = exposure_gdf.merge(aggregated, on="Object ID")
 
-        # Create a string from the list of values in the duplicated aggregation area 
-        # column
-        exposure_gdf[attribute_name] = exposure_gdf[attribute_name].apply(process_value)
+        if len(aggregated[attribute_name].values.any()) >= 2:
+            # If new composite area falls in multiple aggregation zones should do an overlay function and where they overlay, split the composite area into
+            # polygons and assign the aggregation area
+            new_exposure_gdf = exposure_gdf.rename(columns = {attribute_name: "pot"})
+            res_intersection = new_exposure_gdf.overlay(aggregation_gdf[[attribute_name, 'geometry']], how='intersection')
+            res_intersection.drop(["index_right", "pot"], axis = 1, inplace = True)
+
+            # exposure area - res_intersection = left over shape in no zone 
+            exposure_outside_aggregation = new_exposure_gdf.overlay(res_intersection[["geometry"]], how = "symmetric_difference")
+            exposure_outside_aggregation.drop(["index_right", "pot"], axis = 1, inplace = True)
+            exposure_outside_aggregation = exposure_outside_aggregation.dropna()
+
+            # Combine divided objects of new composite area within aggregation zone and outside #TODO create OBJECT IDs
+            exposure_gdf = pd.concat([res_intersection, exposure_outside_aggregation], ignore_index=True)
+            idx_duplicates = exposure_gdf.index[exposure_gdf.duplicated("Object ID") == True]
+            exposure_gdf["Object ID"] = exposure_gdf["Object ID"].astype(int)
+            exposure_gdf.loc[idx_duplicates, "Object ID"] = np.random.choice(range(exposure_gdf["Object ID"].values.max() + 1 ,exposure_gdf["Object ID"].values.max() +1 + len(idx_duplicates)), size=len(idx_duplicates), replace=False)
             
-        # Rename the 'aggregation_attribute' column to 'new_column_name'. Put in 
-        # Documentation that the order the user put the label name must be the order of the gdf
-        exposure_gdf.rename(columns={attribute_name: label_name}, inplace=True)
+            
+            # Create an empty GeoDataFrame and append the exposure data
+            try:
+                new_exposure_aggregation
+            except NameError:
+                data = pd.DataFrame(columns=['geometry'])
+                final_exposure = gpd.GeoDataFrame(data, geometry='geometry')
+                new_exposure_aggregation = pd.concat([final_exposure, exposure_gdf], ignore_index=True) 
+                exposure_gdf = new_exposure_aggregation
+            else:
+                exposure_gdf = pd.concat([new_exposure_aggregation, exposure_gdf], ignore_index=True)            
+        else:
+            exposure_gdf.drop(columns=attribute_name, inplace=True)
+            exposure_gdf = exposure_gdf.merge(aggregated, on="Object ID")
 
-        ##remove the index_right column
-        if "index_right" in exposure_gdf.columns:
-            del exposure_gdf["index_right"]
+            # Create a string from the list of values in the duplicated aggregation area 
+            # column
+            exposure_gdf[attribute_name] = exposure_gdf[attribute_name].apply(process_value)
+                
+            # Rename the 'aggregation_attribute' column to 'new_column_name'. Put in 
+            # Documentation that the order the user put the label name must be the order of the gdf
+            exposure_gdf.rename(columns={attribute_name: label_name}, inplace=True)
+            
+            exposure_gdf
+            ##remove the index_right column
+            if "index_right" in exposure_gdf.columns:
+                del exposure_gdf["index_right"]
 
     return exposure_gdf, filtered_areas
 
