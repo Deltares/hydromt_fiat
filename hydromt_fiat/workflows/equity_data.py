d2d71fdeab575a7efaeffec1bec4350322f83f47
Panos Athanasiou <38324721+panosatha@users.noreply.github.com>
2024-07-23 16:17:47 +0200

    corrected bug with indexing of block groups (#355)

--- hydromt_fiat/workflows/equity_data.py
+++ hydromt_fiat/workflows/equity_data.py
@@ -185,7 +185,7 @@ class EquityData:
             block_groups_list.append(block_groups_shp)
 
         #TODO Save final file as geopackage
-        self.block_groups = gpd.GeoDataFrame(pd.concat(block_groups_list))
+        self.block_groups = gpd.GeoDataFrame(pd.concat(block_groups_list)).reset_index(drop=True)
         
         # Create string from GEO_ID short 
         for index, row,in self.block_groups.iterrows():
