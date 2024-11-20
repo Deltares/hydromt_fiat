c52aad0d8e017ebc54f7a96c37cf7fcf32a507ab
Santonia27 <sarah.rautenbach@deltares.nl>
2024-11-20 14:53:14 +0100

    read spatial joints in read_geoms()

--- hydromt_fiat/fiat.py
+++ hydromt_fiat/fiat.py
@@ -1285,8 +1285,12 @@ class FiatModel(GridModel):
             fns_aggregation = glob.glob(Path(self.root, "geoms", "aggregation_areas","*.geojson").as_posix())
             fns.append(fns_aggregation[0])
         if self.spatial_joins["additional_attributes"]:
-            fns_additional_attributes = glob.glob(Path(self.root, "geoms", "additional_attributes","*.geojson").as_posix())
-            fns.append(fns_additional_attributes[0])
+            if len(glob.glob(Path(self.root, "geoms", "additional_attributes","*.geojson").as_posix())) > 0:
+                fns_additional_attributes = glob.glob(Path(self.root, "geoms", "additional_attributes","*.geojson").as_posix())
+                fns.append(fns_additional_attributes[0])
+            if len(glob.glob(Path(self.root, "geoms", "building_footprints","*.geojson").as_posix())) > 0:
+                fns_building_footprints = glob.glob(Path(self.root, "geoms", "building_footprints","*.geojson").as_posix())
+                fns.append(fns_building_footprints[0])
         if len(fns) >= 1:
             self.logger.info("Reading static geometries")
         for fn in fns:
