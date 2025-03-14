cdd3f61a6818c938183ad474480029d2976ecede
dalmijn <92092029+dalmijn@users.noreply.github.com>
2025-03-14 15:40:39 +0100

    V1 structure (#457)
    * Basic structure
    
    * Add test-data, small setup for testing

---
+++ src/hydromt_fiat/data/fetch.py
@@ -0,0 +1,91 @@
+"""Data for examples and testing of HydroMT-FIAT."""
+
+import json
+import logging
+from pathlib import Path
+
+import pooch
+
+__all__ = ["fetch_data"]
+
+logger = logging.getLogger(__name__)
+
+# update the base URL and registry with new versions of the data
+# use create_artifact.py script to create the build-data/ test-data archives
+with open(Path(__file__).parent / "registry.json", "r") as f:
+    DATABASE = json.load(f)
+    BASE_URL: str = DATABASE["url"]
+    REGISTRY: dict[str:str] = DATABASE["data"]
+CACHE_DIR = Path("~", ".cache", "hydromt_fiat").expanduser()
+PROCESSORS = {
+    "tar.gz": pooch.Untar,
+    "zip": pooch.Unzip,
+}
+
+
+def unpack_processor(
+    suffix: str,
+    extract_dir: str = "./",
+):
+    """Select the right processor for unpacking."""
+    if suffix not in PROCESSORS:
+        return None
+    processor = PROCESSORS[suffix](members=None, extract_dir=extract_dir)
+    return processor
+
+
+def fetch_data(
+    data: str,
+    output_dir: Path | str | None = None,
+    sub_dir: bool = True,
+) -> Path:
+    """Fetch data by simply calling the function.
+
+    Parameters
+    ----------
+    data : str
+        The data to fetch.
+    output_dir : Path | str | None
+        The output directory to store the data.
+        If None, the data will be stored in ~/.cache/hydromt_fiat/<data>
+    sub_dir : bool
+        Whether to place the fetched data in a sub directory of the same name.
+        I.e. if  the (tarred) dataset is named 'custom-data' a directory named
+        'custom-data' is created in which the data are placed. By default True
+
+    Returns
+    -------
+    Path
+        The output directory where the data is stored
+    """
+    CACHE_DIR.mkdir(parents=True, exist_ok=True)
+    if output_dir is None:
+        output_dir = CACHE_DIR
+    else:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Quick check whether the data can be found
+    choices_raw = list(REGISTRY.keys())
+    choices = [item.split(".", 1)[0] for item in choices_raw]
+    if data not in choices:
+        raise ValueError(f"Choose one of the following: {choices}")
+    idx = choices.index(data)
+
+    # Setup Pooch
+    retriever = pooch.create(
+        path=CACHE_DIR,  # store archive to cache
+        base_url=BASE_URL,
+        registry=REGISTRY,
+    )
+
+    # Set the way of unpacking it
+    suffix = choices_raw[idx].split(".", 1)[1]
+    extract_dir = output_dir
+    if sub_dir:
+        extract_dir = Path(extract_dir, data)
+    processor = unpack_processor(suffix, extract_dir=extract_dir)
+    # Retrieve the data
+    retriever.fetch(choices_raw[idx], processor=processor)
+
+    return extract_dir
--- src/hydromt_fiat/fiat.py
+++ src/hydromt_fiat/fiat.py
@@ -1,16 +1,175 @@
 """Main module."""
 
-from hydromt_fiat import utils, workflows
+import logging
+from pathlib import Path
+from typing import List, Union
+
+import geopandas as gpd
+from hydromt.model import Model
+from hydromt.model.components import (
+    ConfigComponent,
+    GeomsComponent,
+    GridComponent,
+    TablesComponent,
+)
+from hydromt.model.steps import hydromt_step
+
+from hydromt_fiat.components import RegionComponent
+
+# Set some global variables
+__all__ = ["FIATModel"]
+__hydromt_eps__ = ["FIATModel"]  # core entrypoints
+
+# Create a logger
+logger = logging.getLogger(__name__)
 
 
-class FIATModel:
-    """_summary_."""
+class FIATModel(Model):
+    """Read or Write a FIAT model.
+
+    Parameters
+    ----------
+    root : str, optional
+        Model root, by default None
+    mode : {'r','r+','w'}, optional
+        read/append/write mode, by default "w"
+    data_libs : List[str], optional
+        List of data catalog configuration files, by default None
+    logger:
+        The logger to be used.
+    **catalog_keys:
+        Additional keyword arguments to be passed down to the DataCatalog.
+    """
+
+    name: str = "fiat_model"
+    # supported model version should be filled by the plugins
+    # e.g. _MODEL_VERSION = ">=1.0, <1.1"
+    _MODEL_VERSION = None
 
     def __init__(
         self,
+        root: str | None = None,
+        mode: str = "r",
+        data_libs: Union[List, str] | None = None,
+        **catalog_keys,
     ):
-        foo = workflows.api
-        bar = utils.bla
-        bar()
-        foo()
+        Model.__init__(
+            self,
+            root,
+            components={"region": RegionComponent(model=self)},
+            mode=mode,
+            region_component="region",
+            data_libs=data_libs,
+            **catalog_keys,
+        )
+
+        ## Setup components
+        self.add_component(
+            "config",
+            ConfigComponent(model=self, filename="settings.toml"),
+        )
+        self.add_component("exposure_data", TablesComponent(model=self))
+        self.add_component(
+            "exposure_geoms",
+            GeomsComponent(model=self, region_component="region"),
+        )
+        self.add_component(
+            "exposure_grid",
+            GridComponent(model=self, region_component="region"),
+        )
+        self.add_component(
+            "hazard_grid",
+            GridComponent(model=self, region_component="region"),
+        )
+        self.add_component("vulnerability_data", TablesComponent(model=self))
+
+    ## Properties
+    @property
+    def config(self):
+        """Return the configurations component."""
+        return self.components["config"]
+
+    ## I/O
+    @hydromt_step
+    def read(self):
+        """Read the FIAT model."""
+        Model.read(self)
+
+    @hydromt_step
+    def write(self):
+        """Write the FIAT model."""
+        Model.write(self)
+
+    ## Setup methods
+    @hydromt_step
+    def setup_config(
+        self,
+        **settings: dict,
+    ):
+        """Set config file entries.
+
+        settings : dict
+            Settings for the configuration provided as keyword arguments
+            (KEY=VALUE).
+        """
+        for key, value in settings.items():
+            self.config.set(key, value)
+
+    @hydromt_step
+    def setup_region(
+        self,
+        region: Path | str,
+    ):
+        """Set the region of the FIAT model.
+
+        Parameters
+        ----------
+        region : Path | str
+            Path to the region vector file.
+        """
+        region = Path(region)
+        if not region.is_file():
+            raise FileNotFoundError(region.as_posix())
+        geom = gpd.read_file(region)
+        self.components["region"].set(geom, "region")
+
+    @hydromt_step
+    def setup_exposure(
+        self,
+        exposure_fname: Path | str,
+    ):
+        """Set up the exposure from a data source.
+
+        Parameters
+        ----------
+        exposure_fname : Path | str
+            _description_
+        """
+        pass
+
+    @hydromt_step
+    def setup_hazard(
+        self,
+        hazard_fname: Path,
+    ):
+        """Set up hazard from a data source.
+
+        Parameters
+        ----------
+        hazard_fname : Path
+            _description_
+        """
+        pass
+
+    def setup_vulnerability(
+        self,
+        vuln_fname: Path | str,
+    ):
+        """Set up the vulnerability from a data source.
+
+        Parameters
+        ----------
+        vuln_fname : Path | str
+            _description_
+        """
         pass
