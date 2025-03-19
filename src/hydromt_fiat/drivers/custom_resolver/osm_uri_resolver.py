9d38c95950bf0fab04bc0b7e4925d5c393060a50
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-03-19 14:39:23 +0100

    addded custom resolver to handle osm uri

---
+++ src/hydromt_fiat/drivers/custom_resolver/osm_uri_resolver.py
@@ -0,0 +1,16 @@
+"""Resolve uri argument for OSM data catalog sources."""
+
+from pathlib import Path
+
+from hydromt.data_catalog.uri_resolvers import URIResolver
+
+
+class OSMResolver(URIResolver):
+    """Resolve uri argument for OSM data catalog sources."""
+
+    name = "osm_resolver"
+
+    def resolve(self, uri, **kwargs) -> list[str]:
+        """Resolve OSM uri argument."""
+        uri = Path(uri).stem
+        return [uri]
