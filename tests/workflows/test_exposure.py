33f971ff6706a7b78587c9b705c756bcdac601c5
Tjalling-dejong <tjalling.dejong@deltares.nl>
2025-04-01 11:14:33 +0200

    added exposure grid tests

--- tests/workflows/test_exposure.py
+++ tests/workflows/test_exposure.py
@@ -1,20 +1,19 @@
-from pathlib import Path
+import logging
 
 import pandas as pd
+import xarray as xr
 from hydromt import DataCatalog
 
 from hydromt_fiat.workflows import exposure_grid_data
 
 
-def test_exposure_grid_data(build_data_catalog, build_region_gdf, tmp_path):
+def test_exposure_grid_data(build_data_catalog, build_region_gdf, tmp_path, caplog):
     dc = DataCatalog(build_data_catalog)
-    raster_source = dc.get_source("flood_event")
-    file_name = Path(raster_source.uri).name
     linking_table = pd.DataFrame(
-        data=[{"exposure": file_name, "vulnerability": "damage_function_file"}]
+        data=[{"exposure": "flood_event", "vulnerability": "damage_function_file"}]
     )
     linking_table_fp = tmp_path / "linking_table.csv"
-    linking_table.to_csv(linking_table_fp)
+    linking_table.to_csv(linking_table_fp, index=False)
 
     ds = exposure_grid_data(
         grid_like=None,
@@ -23,4 +22,28 @@ def test_exposure_grid_data(build_data_catalog, build_region_gdf, tmp_path):
         exposure_files=["flood_event"],
         linking_table=linking_table_fp,
     )
-    assert ds
+    assert isinstance(ds, xr.Dataset)
+    assert ds.attrs.get("damage_function") == "damage_function_file"
+
+    # Test without matching exposure file name in linking table
+    caplog.set_level(logging.WARNING)
+    linking_table = pd.DataFrame(
+        data=[{"exposure": "event", "vulnerability": "damage_function_file"}]
+    )
+    linking_table.to_csv(linking_table_fp, index=False)
+
+    ds = exposure_grid_data(
+        grid_like=None,
+        region=build_region_gdf,
+        data_catalog=dc,
+        exposure_files=["flood_event"],
+        linking_table=linking_table_fp,
+    )
+    log_msg = (
+        "Exposure file name, 'flood_event', not found in linking table."
+        " Setting damage curve file name attribute to 'flood_event'."
+    )
+    assert log_msg in caplog.text
+
+    # Check if damage function defaults to exposure file name
+    assert ds.attrs.get("damage_function") == "flood_event"
