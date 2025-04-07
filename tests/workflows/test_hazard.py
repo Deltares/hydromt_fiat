37bf7b89895ee7d303e073f3e15ded5a58f3b2ac
dalmijn <92092029+dalmijn@users.noreply.github.com>
2025-04-07 12:35:59 +0200

    V1 Units fix (#491)
    * Add units properly; update tests; minor vuln fix
    
    * Dont assert on floats
    
    * Code smell
    
    * Address comments

--- tests/workflows/test_hazard.py
+++ tests/workflows/test_hazard.py
@@ -1,3 +1,4 @@
+import numpy as np
 import xarray as xr
 
 from hydromt_fiat.workflows import hazard_data
@@ -36,3 +37,39 @@ def test_hazard_data_event(build_region_gdf, data_catalog):
     assert ds.analysis == "event"
     assert ds.name == ["flood_event"]
     assert "return_period" not in ds.attrs.keys()
+
+
+def test_hazard_data_unit_default(
+    build_region_gdf,
+    data_catalog,
+):
+    hazard_files = ["flood_event"]
+    ds = hazard_data(
+        grid_like=None,
+        data_catalog=data_catalog,
+        hazard_fnames=hazard_files,
+        hazard_type="flooding",
+        region=build_region_gdf,
+    )
+
+    avg_level = ds.flood_event.mean().values
+    assert np.isclose(avg_level, 1.2019)
+
+
+def test_hazard_data_unit_differ(build_region_gdf, data_catalog, caplog):
+    hazard_files = ["flood_event"]
+    # Suppose it's in a different unit
+    ds = hazard_data(
+        grid_like=None,
+        data_catalog=data_catalog,
+        hazard_fnames=hazard_files,
+        hazard_type="flooding",
+        region=build_region_gdf,
+        unit="ft",
+    )
+
+    avg_level_ft = ds.flood_event.mean().values
+    assert (
+        "Given unit (ft) does not match the standard unit (m) for length" in caplog.text
+    )
+    assert np.isclose(avg_level_ft, 0.366337)
