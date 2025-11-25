01470153585ecdc04679a58b772d812273fd96a1
dalmijn <brencodeert@outlook.com>
2025-11-25 17:28:18 +0100

    Update typing

--- src/hydromt_fiat/gis/raster_utils.py
+++ src/hydromt_fiat/gis/raster_utils.py
@@ -5,13 +5,18 @@ import xarray as xr
 
 def force_ns(
     ds: xr.Dataset | xr.DataArray,
-):
+) -> xr.Dataset | xr.DataArray:
     """Force a raster in north-south orientation.
 
     Parameters
     ----------
     ds : xr.Dataset | xr.DataArray
         The input dataset to check.
+
+    Returns
+    -------
+    xr.Dataset | xr.DataArray
+        Data in north-south orientation.
     """
     if ds.raster.res[1] > 0:
         ds = ds.raster.flipud()
