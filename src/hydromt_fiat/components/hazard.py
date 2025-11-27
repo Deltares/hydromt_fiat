31cd58c3fd0c21e414fe0c7bb138a711db9c20a0
dalmijn <92092029+dalmijn@users.noreply.github.com>
2025-11-27 12:35:52 +0100

    Fix: north south orientation (#563)
    * Fix north south orientation
    
    * Small new gis module
    
    * Fix dumb test mistake
    
    * Update typing
    
    * Housekeeping
    
    * Intermediate grid component
    
    * Harmonize docstrings a bit more

--- src/hydromt_fiat/components/hazard.py
+++ src/hydromt_fiat/components/hazard.py
@@ -4,15 +4,16 @@ import logging
 from pathlib import Path
 from typing import Any
 
-import geopandas as gpd
-import xarray as xr
+from hydromt._io.readers import _read_nc
 from hydromt._io.writers import _write_nc
 from hydromt.model import Model
-from hydromt.model.components import GridComponent
 from hydromt.model.steps import hydromt_step
 
 from hydromt_fiat import workflows
+from hydromt_fiat.components.grid import CustomGridComponent
 from hydromt_fiat.errors import MissingRegionError
+from hydromt_fiat.gis.raster_utils import force_ns
+from hydromt_fiat.gis.utils import crs_representation
 from hydromt_fiat.utils import (
     GRID,
     HAZARD,
@@ -23,7 +24,6 @@ from hydromt_fiat.utils import (
     REGION,
     SRS,
     VAR_AS_BAND,
-    srs_representation,
 )
 
 __all__ = ["HazardComponent"]
@@ -31,7 +31,7 @@ __all__ = ["HazardComponent"]
 logger = logging.getLogger(f"hydromt.{__name__}")
 
 
-class HazardComponent(GridComponent):
+class HazardComponent(CustomGridComponent):
     """Hazard component.
 
     Inherits from the HydroMT-core GridComponent model-component.
@@ -86,6 +86,10 @@ class HazardComponent(GridComponent):
         **kwargs : dict
             Additional keyword arguments to be passed to the `read_nc` method.
         """
+        # Check the state
+        self.root._assert_read_mode()
+        self._initialize_grid(skip_read=True)
+
         # Sort the filename
         # Hierarchy: 1) signature, 2) config file, 3) default
         filename = (
@@ -93,10 +97,21 @@ class HazardComponent(GridComponent):
             or self.model.config.get(HAZARD_FILE, abs_path=True)
             or self._filename
         )
+
         # Read the data
         read_path = Path(self.root.path, filename)
         logger.info(f"Reading the hazard file at {read_path.as_posix()}")
-        super().read(filename=read_path, **kwargs)
+        # Read with the (old) read function from hydromt-core
+        ncs = _read_nc(
+            read_path,
+            self.root.path,
+            single_var_as_array=False,
+            mask_and_scale=False,
+            **kwargs,
+        )
+        # Set the datasets
+        for ds in ncs.values():
+            self.set(ds)
 
     @hydromt_step
     def write(
@@ -133,6 +148,8 @@ class HazardComponent(GridComponent):
 
         # Write it in a gdal compliant manner by default
         logger.info(f"Writing the hazard data to {write_path.as_posix()}")
+        # Force north south before writing
+        self._data = force_ns(self.data)  # type: ignore[assignment]
         _write_nc(
             {GRID: self.data},
             write_path.as_posix(),
@@ -153,53 +170,8 @@ class HazardComponent(GridComponent):
         # Set the srs
         self.model.config.set(
             f"{HAZARD_SETTINGS}.{SRS}",
-            srs_representation(self.data.raster.crs),
+            crs_representation(self.data.raster.crs),
         )
-
-    ## Mutating methods
-    @hydromt_step
-    def clear(self):
-        """Clear the hazard data."""
-        self._data = None
-        self._initialize_grid(skip_read=True)
-
-    @hydromt_step
-    def clip(
-        self,
-        geom: gpd.GeoDataFrame,
-        buffer: int = 1,
-        inplace: bool = False,
-    ) -> xr.Dataset | None:
-        """Clip the hazard data based on geometry.
-
-        Parameters
-        ----------
-        geom : gpd.GeoDataFrame
-            The area to clip the data to.
-        buffer : int, optional
-            A buffer of cells around the clipped area to keep, by default 1.
-        inplace : bool, optional
-            Whether to do the clipping in place or return a new xr.Dataset,
-            by default False.
-
-        Returns
-        -------
-        xr.Dataset | None
-            Return a dataset if the inplace is False.
-        """
-        # Check whether it has the necessary dims
-        try:
-            self.data.raster.set_spatial_dims()
-        except ValueError:
-            return None
-
-        # If so, clip the data
-        data = self.data.raster.clip_geom(geom, buffer=buffer)
-        # If inplace, just set the data and return nothing
-        if inplace:
-            self._data = data
-            return None
-        return data
 
     # Setup methods
     @hydromt_step
--- tests/components/test_hazard_component.py
+++ tests/components/test_hazard_component.py
@@ -2,7 +2,6 @@ import logging
 from pathlib import Path
 from unittest.mock import MagicMock, PropertyMock
 
-import geopandas as gpd
 import pytest
 import xarray as xr
 from hydromt.model import ModelRoot
@@ -30,78 +29,6 @@ def test_hazard_component_empty(
     assert component._filename == f"{HAZARD}.nc"
     assert len(component.data) == 0
     assert isinstance(component.data, xr.Dataset)
-
-
-def test_hazard_component_clear(
-    mock_model: MagicMock,
-    hazard: xr.Dataset,
-):
-    # Set up the component
-    component = HazardComponent(model=mock_model)
-
-    # Set data like a dummy
-    component._data = hazard
-    # Assert the current state
-    assert len(component.data.data_vars) == 1
-
-    # Call the clear method
-    component.clear()
-    # Assert the state after
-    assert len(component.data.data_vars) == 0
-
-
-def test_hazard_component_clip(
-    mock_model: MagicMock,
-    build_region_small: gpd.GeoDataFrame,
-    hazard: xr.Dataset,
-):
-    # Set up the component
-    component = HazardComponent(model=mock_model)
-
-    # Set data like a dummy
-    component._data = hazard
-    # Assert the current state
-    assert component.data.flood_event.shape == (34, 25)
-
-    # Call the clipping method using a smaller region
-    ds = component.clip(geom=build_region_small, buffer=0)
-    # Assert the output
-    assert ds.flood_event.shape == (5, 4)
-
-
-def test_hazard_component_clip_no_data(
-    mock_model: MagicMock,
-    build_region_small: gpd.GeoDataFrame,
-):
-    # Set up the component
-    component = HazardComponent(model=mock_model)
-    # Assert the current state
-    assert component._data is None
-
-    # Call the clipping method using a smaller region
-    ds = component.clip(geom=build_region_small)
-    # Assert that there is no output
-    assert ds is None
-
-
-def test_hazard_component_clip_inplace(
-    mock_model: MagicMock,
-    build_region_small: gpd.GeoDataFrame,
-    hazard: xr.Dataset,
-):
-    # Set up the component
-    component = HazardComponent(model=mock_model)
-
-    # Set data like a dummy
-    component._data = hazard
-    # Assert the current state
-    assert component.data.flood_event.shape == (34, 25)
-
-    # Call the clipping method using a smaller region
-    ds = component.clip(geom=build_region_small, buffer=0, inplace=True)
-    # Assert that the output is None but the shape of the component data changed
-    assert ds is None
-    assert component.data.flood_event.shape == (5, 4)
 
 
 def test_hazard_component_read(
