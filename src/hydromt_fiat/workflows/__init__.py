a7bf3bde4c229b98d98f0406e5df62c0a61b3d52
Panos Athanasiou <panos.athanasiou@deltares.nl>
2026-05-13 15:25:37 +0200

    added merge option for vulnerability and helper methods

--- src/hydromt_fiat/workflows/__init__.py
+++ src/hydromt_fiat/workflows/__init__.py
@@ -9,7 +9,12 @@ from .exposure_geom import (
 )
 from .exposure_grid import exposure_grid_setup
 from .hazard import hazard_setup
-from .vulnerability import process_vulnerability_linking, vulnerability_setup
+from .vulnerability import (
+    merge_vulnerability_curves,
+    merge_vulnerability_linking,
+    process_vulnerability_linking,
+    vulnerability_setup,
+)
 
 __all__ = [
     "aggregate_vector_grid",
@@ -19,6 +24,8 @@ __all__ = [
     "exposure_grid_setup",
     "hazard_setup",
     "max_monetary_damage",
+    "merge_vulnerability_curves",
+    "merge_vulnerability_linking",
     "process_vulnerability_linking",
     "vulnerability_setup",
 ]
