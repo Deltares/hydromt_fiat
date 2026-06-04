95e0594a48006159cb7f8e513803a162d74fca4a
dalmijn <brencodeert@outlook.com>
2026-06-04 08:48:08 +0200

    Fix silly mistake

--- src/hydromt_fiat/data/get.py
+++ src/hydromt_fiat/data/get.py
@@ -171,7 +171,7 @@ def fetch_data(
     # Use common cache directory or a user provided one
     cache_dir = cache_dir or CACHE_DIR
     # Set the output_dir
-    output_dir = Path(Path.cwd(), output_dir) or cache_dir
+    output_dir = Path(Path.cwd(), output_dir or cache_dir)
     if sub_dir:
         output_dir = Path(output_dir, data.split(".", 1)[0])
 
